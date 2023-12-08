84644da945e6e8065154b81707d57e684bb98369
misaif <107899521+misaif@users.noreply.github.com>
2023-12-08 15:29:15 +0800

    Update nodeseek.py

--- nodeseek.py
+++ nodeseek.py
@@ -14,7 +14,7 @@ cookie = os.getenv('NODESEEK_COOKIE')
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1 Edg/119.0.0.0',
-    'Cookie': 'session=' + cookie
+    'Cookie': cookie
 }
 
 
