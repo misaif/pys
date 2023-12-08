0f24dda9197f86d41d24a462223ce2f6161c0a7b
misaif <107899521+misaif@users.noreply.github.com>
2023-12-08 15:37:22 +0800

    Update nodeseek.py

--- nodeseek.py
+++ nodeseek.py
@@ -13,17 +13,17 @@ import requests
 cookie = os.getenv('NODESEEK_COOKIE')
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-    'Cookie': cookie
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1 Edg/119.0.0.0',
+    'Cookie': 'session=' + cookie
 }
 
 
-def nodeseek():
-    print(headers)
+def attendance():
     url = 'https://www.nodeseek.com/api/attendance?random=true'
-    res = requests.post(url, headers=headers)
-    print(res)
+    res = requests.post(url, headers=headers).json()
+    message = res['message']
+    send('NodeSeek Checkin', message)
 
 
 if __name__ == '__main__':
-    nodeseek()
+    attendance()
